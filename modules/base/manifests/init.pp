7c5d44aeed8e2adbd583bdaa9a28c25a87cc3c8e
Alain <a.fernandez.arrieta@accenture.com>
2014-03-31 17:59:39 +0200

    Ejecuta helloworld una vez se asegura que el servicio de Tomcat está levantado. Helloworld trae el artefacto del repositorio Nexus.

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -9,8 +9,18 @@ class base{
     require => Package['tomcat6'],
   }
   
- package { 'git':
-   ensure => installed,
- }
-
+  package { 'git':
+    ensure => installed,
+  }
+  
+  file { '/tmp/helloworld.sh':
+    source  => "puppet:///modules/base/helloworld.sh",
+    mode    => "700",
+    require => Service['tomcat6'],
+  }
+  
+  exec { 'helloworld':
+    command => 'sh /tmp/helloworld.sh',
+    require => File['/tmp/helloworld.sh']
+  }
 }
