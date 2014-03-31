35d9162c73f92bfa8b86994d27f6623cc58be268
Alain <a.fernandez.arrieta@accenture.com>
2014-03-31 18:50:53 +0200

    Correcciones helloworld.sh ¡¡qué a gusto nos vamos a quedar cuando lo renombremos y se pierda su puta historia!

--- modules/base/files/helloworld.sh
+++ modules/base/files/helloworld.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Download the latest version of an artifact available in a repository (by its ga[v]r)
-FIND_GROUPID=acn.bbva.gmelabs
-FIND_ARTIFACTID=test
-FIND_REPOSITORYID=snapshots 
+FIND_GROUPID=acn.b.gmelabs
+FIND_ARTIFACTID=testweb
+FIND_REPOSITORYID=snapshots
 
-FIND_PATH=`curl -X GET -u admin:admin123 -H "Accept: application/xml" "http://localhost:8081/nexus/service/local/artifact/maven/resolve?g=${FIND_GROUPID}&a=${FIND_ARTIFACTID}&v=LATEST&r=${FIND_REPOSITORYID} <http://localhost:8081/nexus/service/local/artifact/maven/resolve?g=%24%7bFIND_GROUPID%7d&a=%24%7bFIND_ARTIFACTID%7d&v=LATEST&r=%24%7bFIND_REPOSITORYID%7d> " 2>/dev/null | gawk -F'[<>]' '/\<repositoryPath\>/{ print $3 }'`
-wget "http://localhost:8081/nexus/service/local/repositories/${FIND_REPOSITORYID}/content/${FIND_PATH} <http://localhost:8081/nexus/service/local/repositories/%24%7bFIND_REPOSITORYID%7d/content/%24%7bFIND_PATH%7d> " 2>/dev/null
+FIND_PATH=`curl -X GET -u admin:admin123 -H "Accept: application/xml" "http://10.192.104.11:8081/nexus/service/local/artifact/maven/resolve?g=${FIND_GROUPID}&a=${FIND_ARTIFACTID}&v=LATEST&r=${FIND_REPOSITORYID}" 2>/dev/null | gawk -F'[<>]' '/\<repositoryPath\>/{ print $3 }'`
+wget "http://10.192.104.11:8081/nexus/service/local/repositories/${FIND_REPOSITORYID}/content/${FIND_PATH}" 2>/dev/null
