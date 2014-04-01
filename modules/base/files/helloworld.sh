e7a6386ef9d42b7c2d6947f1b64a8c50f7041561
Alain <a.fernandez.arrieta@accenture.com>
2014-04-01 08:52:32 +0200

    Correciones helloworld.sh

--- modules/base/files/helloworld.sh
+++ modules/base/files/helloworld.sh
@@ -5,5 +5,5 @@ FIND_GROUPID=acn.b.gmelabs
 FIND_ARTIFACTID=testweb
 FIND_REPOSITORYID=snapshots
 
-FIND_PATH=`curl -X GET -u admin:admin123 -H "Accept: application/xml" "http://10.192.104.11:8081/nexus/service/local/artifact/maven/resolve?g=${FIND_GROUPID}&a=${FIND_ARTIFACTID}&v=LATEST&r=${FIND_REPOSITORYID}" 2>/dev/null | gawk -F'[<>]' '/\<repositoryPath\>/{ print $3 }'`
-wget "http://10.192.104.11:8081/nexus/service/local/repositories/${FIND_REPOSITORYID}/content/${FIND_PATH}" 2>/dev/null
+FIND_PATH=`curl -X GET -u admin:admin123 -H "Accept: application/xml" "http://10.192.104.12:8081/nexus/service/local/artifact/maven/resolve?g=${FIND_GROUPID}&a=${FIND_ARTIFACTID}&v=LATEST&r=${FIND_REPOSITORYID}" 2>/dev/null | gawk -F'[<>]' '/\<repositoryPath\>/{ print $3 }'`
+cd /var/lib/tomcat6/webapps && wget "http://10.192.104.12:8081/nexus/service/local/repositories/${FIND_REPOSITORYID}/content/${FIND_PATH}" 2>/dev/null
